9c50ac105e3323e8296c4ecb123946e5cf70145b
Haikez <1937086590@qq.com>
2020-10-08 12:31:24 +0800

    Okk

--- raspi-vim/.vim/pydiction/a.py
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (C) 2019-2019 XXX Technology Co., Ltd. All rights reserved.
- * 
- * File Name: a.py
- * Author: NiChu
- * Mail: 1937086590@qq.com
- * Created Time: 2020-10-05 12:16
- */
-print("hello")
