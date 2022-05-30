289865aee662410ff0fb0cfdfd53be2b6a9f83d8
Haikez <1937086590@qq.com>
2022-05-30 12:16:52 +0100

    修改了install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
-
+#!/bin/bash
+cp .vimrc ~/.vimrc
+cp -r ./vim/.vim ~/.vim
+vim -c 'PlugInstall'
+read -p "If you remove the installation package?[y/n]" input
+case $input in
+	[yY]*)
+		cd ../
+		sudo rm -r vim-config
+		echo "Delete Successfully"
+		;;
+	[nN]*)
+		exit
+		;;
+	*)
+		echo "Just enter y or n,please."
+		exit
+		;;
+esac		
